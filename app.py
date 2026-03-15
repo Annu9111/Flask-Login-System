1d316264f47db85765e9d2ba94eeeaa4850204d4
Annu9111 <aksoni911190@gmail.com>
2026-03-15 09:17:58 +0530

    contact

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello User!!\n This is a Flask app" 
+    return "Hello User!!\n This is a Flask app"
+
+@app.route("/about")
+def about():
+    return "this is about page"
+
+@app.route("/contact")
+def contact():
+    return "this is contact page" 
